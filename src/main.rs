ede7ad1a6386db96c1a9ceccb42d4a4729440560
Jacob Chvatal <jake@isnt.online>
2023-01-07 02:15:07 -0500

    added fuzzy matching window

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 use eframe::egui;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 240.0)),
         ..Default::default()
     };
-    eframe::run_native(
-        "egui example: custom font",
-        options,
-        Box::new(|cc| Box::new(MyApp::new(cc))),
-    )
+    eframe::run_native("launch", options, Box::new(|cc| Box::new(MyApp::new(cc))))
 }
 
 struct MyApp {
     query: String,
     options: Vec<String>,
+    matcher: SkimMatcherV2,
+    idx: u8,
 }
 
 impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
-            options: vec!["a".into(), "b".into(), "c".into()],
+            options: vec!["Firefox".into(), "Amazon".into(), "Google Chrome".into()],
             query: "".to_owned(),
+            matcher: SkimMatcherV2::default(),
+            idx: 0,
         }
     }
 }
@@ -32,14 +32,20 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("egui using custom fonts");
-            ui.text_edit_multiline(&mut self.query);
+            ui.text_edit_singleline(&mut self.query);
             self.options
                 .iter()
-                .filter(|opt| self.query.eq(*opt))
+                .filter(|opt| self.matcher.fuzzy_match(opt, &self.query).is_some())
                 .for_each(|x| {
                     ui.heading(x);
-                })
+                });
         });
+
+        // idx starts at launching app 0.
+        // if the arrow keys go down, we select the next one
+        // if the arrow keys go up, we wrap to the previous
+        // if enter is pressed, run the current idx item
+        // otherwise, enter into the text box
 
         _frame.set_fullscreen(true);
     }
