b131bb53328d2d9762dcc00ace0369def25bb48e
Jacob Chvatal <jake@isnt.online>
2023-01-09 21:43:42 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -28,33 +28,6 @@ fn find_desktop_entries() -> Vec<Entry> {
     entries
 }
 
-#[inline]
-fn heading2() -> TextStyle {
-    TextStyle::Name("Heading2".into())
-}
-
-#[inline]
-fn heading3() -> TextStyle {
-    TextStyle::Name("ContextHeading".into())
-}
-
-fn configure_text_styles(ctx: &egui::Context) {
-    use FontFamily::{Monospace, Proportional};
-
-    let mut style = (*ctx.style()).clone();
-    style.text_styles = [
-        (TextStyle::Heading, FontId::new(25.0, Proportional)),
-        (heading2(), FontId::new(22.0, Proportional)),
-        (heading3(), FontId::new(19.0, Proportional)),
-        (TextStyle::Body, FontId::new(16.0, Proportional)),
-        (TextStyle::Monospace, FontId::new(12.0, Monospace)),
-        (TextStyle::Button, FontId::new(12.0, Proportional)),
-        (TextStyle::Small, FontId::new(8.0, Proportional)),
-    ]
-    .into();
-    ctx.set_style(style);
-}
-
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         // decorated: false,
@@ -94,7 +67,6 @@ struct Launch {
 
 impl Launch {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        configure_text_styles(&_cc.egui_ctx);
         Self {
             options: find_desktop_entries(),
             query: "".to_owned(),
@@ -144,9 +116,9 @@ impl eframe::App for Launch {
                 .show(ui, |ui| {
                     opts.iter().enumerate().for_each(|(i, entry)| {
                         if i == self.idx {
-                            ui.label(RichText::new(&entry.name).text_style(heading2()).strong());
+                            ui.label(RichText::new(&entry.name).strong());
                         } else {
-                            ui.label(RichText::new(&entry.name).text_style(heading2()));
+                            ui.label(RichText::new(&entry.name));
                         }
                     });
                 });
